3df54c2219183d01afc9c4b8e7745d04df063997
devarsh2007 <sunilpatel2131@gmail.com>
2024-05-13 19:51:33 +0530

    loop

---
+++ while1.py
@@ -0,0 +1,17 @@
+# write a program that print 1 to 100
+
+number = 0
+
+while number<10:
+      number=number+1
+      print(number)
+
+
+# number = number+1  #number = 1+1
+# print(number)
+
+# number = number+1 # number = 2+1
+# print(number)
+
+# number=number+1  #3+1
+# print(number)
