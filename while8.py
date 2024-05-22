ff9b57fa826d3428dc8f24771c659ae99382cc07
devarsh2007 <sunilpatel2131@gmail.com>
2024-05-22 19:55:38 +0530

    code

---
+++ while8.py
@@ -0,0 +1,25 @@
+a=input("enter message : ")
+
+name = {}
+l1=[]
+
+t1=()
+t1=list(t1)
+
+name=set(name)
+
+last = len(a)-1
+while last>=0:
+      print(a[last])
+      name.add(a[last])
+      l1.append(a[last])
+      
+      t1.append(a[last])
+
+      last-=1
+
+t1=tuple(t1)
+
+print("set : ",name)
+print("list : ",l1)
+print("tuple : ",t1)
