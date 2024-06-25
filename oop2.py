9686e64e09922f02d48e32495189223f6049e2b9
devarsh2007 <sunilpatel2131@gmail.com>
2024-06-25 19:53:28 +0530

    oop

---
+++ oop2.py
@@ -0,0 +1,20 @@
+class employee():
+      def work(self):
+            print("i can work")
+            
+      def talk(self):
+            print("i can talk")
+            
+      def walk(self):
+            print("i can walk")
+            
+
+obj = employee()
+obj.work()
+obj.talk()
+obj.walk()
+
+e1 = employee()
+e1.work()
+
+obj.work()
