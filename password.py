463a35dc4f0726b1acbc2cc7f73fa0958c09bfe8
devarsh2007 <sunilpatel2131@gmail.com>
2024-07-03 08:17:47 +0530

    password

--- password.py
+++ password.py
@@ -6,9 +6,19 @@ import random as r
 
 from random import *
 
+
+
+l1 = [
+    '0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
+    'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
+    'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+    '!', '@', '#', '$', '%' '_',
+]
+
 password = ""
 for i in range(10):
-      password+=str(randint(0,9))
+      # password+=str(randint(0,9))
+      password += choice(l1)
 print(password)
 
 # a="1"
